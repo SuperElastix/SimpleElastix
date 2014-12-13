315f0098b0d3a5139f50da7ab6b3216882b8bfd1
kaspermarstal <kaspermarstal@gmail.com>
2014-12-13 17:59:48 +0000

    Added parameter file unit test, C#, CXX, Java examples

---
+++ Examples/SimpleElastix/SimpleElastix.java
@@ -0,0 +1,35 @@
+import org.itk.simple.*;
+
+class SimpleElastix {
+ 
+  public static void main(String argv[]) {
+
+    if ( argv.length < 4 ) {
+      System.out.println("Usage: java SimpleElastix <fixedImage> <movingImage> <parameterFile> <output>");
+      return;
+    }
+
+    // Instantiate SimpleElastix
+    org.itk.simple.SimpleElastix elastix = new org.itk.simple.SimpleElastix();
+
+    // Read input
+    org.itk.simple.ImageFileReader reader = new org.itk.simple.ImageFileReader();
+    reader.setFileName(argv[0]);
+    elastix.SetFixedImage(reader.execute());
+
+    reader.setFileName(argv[1]);
+    elastix.SetMovingImage(reader.execute());
+
+    elastix.SetParameterMap(ReadParameterFile(argv[2]));
+
+    // Perform registration
+    elastix.execute();
+
+    // Write result iamge
+    ImageFileWriter writer = new ImageFileWriter();
+    writer.setFileName(argv[3]);
+    writer.execute(elastix.GetResultImage()); 
+
+  }
+
+}
--- Testing/Unit/simpleelastix.py
+++ Testing/Unit/simpleelastix.py
@@ -12,6 +12,44 @@ class TestSimpleElastix(unittest.TestCase):
         # Make sure SimpleElastix is available from this SimpleITK module
         self.assertTrue("SimpleElastix" in dir(sitk))
 
+    def test_parametermap(self):
+        p = sitk.ParameterMap()
+        p['key'] = 'value'
+        self.assertTrue(p.has_key('key')
+        self.assertTrue(p['key'] == 'value')
+
+    def test_parametermaplist(self):
+        plist = sitk.ParameterMapList()
+        p = sitk.ParameterMap()
+
+        plist.push_back(p)
+        plist.push_back(p)
+        self.assertTrue(plist.size() == 2)
+
+        plist.pop()
+        self.assertTrue(plist.size() == 1)
+
+    def test_defaultparametermaps(self):
+        plist = sitk.ParameterMapList()
+
+        a = sitk.GetDefaultParameterMap('rigid')
+        plist.push_back(a)
+        self.assertTrue(plist[0]['Metric'] == 'EulerTransform')
+
+        b = sitk.GetDefaultParameterMap('affine')
+        plist.push_back(b)
+        self.assertTrue(plist[1]['Metric'] == 'AffineTransform')
+
+        c = sitk.GetDefaultParameterMap('nonrigid')
+        plist.push_back(c)
+        self.assertTrue(plist[2]['Metric'][0] == 'BSplineTransform')
+        self.assertTrue(plist[2]['Metric'][1] == 'TransformBendingEnergyPenalty')
+
+        d = sitk.GetDefaultParameterMap('groupwise')
+        plist.push_back(d)
+        self.assertTrue(plist[3]['Metric'] == 'VarianceOverLastDimensionMetric')
+
+
 if __name__ == '__main__':
     unittest.main()
     
