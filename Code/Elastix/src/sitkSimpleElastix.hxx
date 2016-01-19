551c4fc240bd0b16298418d9b5fce3d3f03e885f
Kasper Marstal <kaspermarstal@gmail.com>
2016-01-19 09:35:30 +0100

    ENH: Catch all ITK exceptions from ElastixFilter and TransformixFilter

--- Code/Elastix/src/sitkSimpleElastix.hxx
+++ Code/Elastix/src/sitkSimpleElastix.hxx
@@ -13,58 +13,56 @@ SimpleElastix::DualExecuteInternal( void )
   typedef elastix::ElastixFilter< TFixedImage, TMovingImage > ElastixFilterType;
   typedef typename ElastixFilterType::Pointer                 ElastixFilterPointer;
 
-  ElastixFilterPointer elastixFilter = ElastixFilterType::New();
-
-  for( unsigned int i = 0; i < this->m_FixedImages.size(); ++i )
-  {
-    elastixFilter->AddFixedImage( static_cast< TFixedImage* >( this->GetFixedImage( i ).GetITKBase() ) );
-  }
-
-  for( unsigned int i = 0; i < this->m_MovingImages.size(); ++i )
-  {
-    elastixFilter->AddMovingImage( static_cast< TMovingImage* >( this->GetMovingImage( i ).GetITKBase() ) );
-  }
-
-  for( unsigned int i = 0; i < this->m_FixedMasks.size(); ++i )
-  {
-    elastixFilter->AddFixedMask( static_cast< TFixedImage* >( this->GetFixedMask( i ).GetITKBase() ) );
-  }
-
-  for( unsigned int i = 0; i < this->m_MovingMasks.size(); ++i )
-  {
-    elastixFilter->AddMovingMask( static_cast< TMovingImage* >( this->GetMovingMask( i ).GetITKBase() ) );
-  }
-
-  elastixFilter->SetFixedPointSetFileName( this->GetFixedPointSetFileName() );
-  elastixFilter->SetMovingPointSetFileName( this->GetMovingPointSetFileName() );
-
-  elastixFilter->SetOutputDirectory( this->GetOutputDirectory() );
-  elastixFilter->SetLogFileName( this->GetLogFileName() );
-  elastixFilter->SetLogToFile( this->GetLogToFile() );
-  elastixFilter->SetLogToConsole( this->GetLogToConsole() );
-
-  if( this->m_ParameterMapVector.size() > 0 )
-  {
-    ParameterObjectPointer parameterObject = ParameterObjectType::New();
-    parameterObject->SetParameterMap( this->m_ParameterMapVector );
-    elastixFilter->SetParameterObject( parameterObject );
-  }
-
   try
   {
+    ElastixFilterPointer elastixFilter = ElastixFilterType::New();
+
+    for( unsigned int i = 0; i < this->m_FixedImages.size(); ++i )
+    {
+      elastixFilter->AddFixedImage( static_cast< TFixedImage* >( this->GetFixedImage( i ).GetITKBase() ) );
+    }
+
+    for( unsigned int i = 0; i < this->m_MovingImages.size(); ++i )
+    {
+      elastixFilter->AddMovingImage( static_cast< TMovingImage* >( this->GetMovingImage( i ).GetITKBase() ) );
+    }
+
+    for( unsigned int i = 0; i < this->m_FixedMasks.size(); ++i )
+    {
+      elastixFilter->AddFixedMask( static_cast< TFixedImage* >( this->GetFixedMask( i ).GetITKBase() ) );
+    }
+
+    for( unsigned int i = 0; i < this->m_MovingMasks.size(); ++i )
+    {
+      elastixFilter->AddMovingMask( static_cast< TMovingImage* >( this->GetMovingMask( i ).GetITKBase() ) );
+    }
+
+    elastixFilter->SetFixedPointSetFileName( this->GetFixedPointSetFileName() );
+    elastixFilter->SetMovingPointSetFileName( this->GetMovingPointSetFileName() );
+
+    elastixFilter->SetOutputDirectory( this->GetOutputDirectory() );
+    elastixFilter->SetLogFileName( this->GetLogFileName() );
+    elastixFilter->SetLogToFile( this->GetLogToFile() );
+    elastixFilter->SetLogToConsole( this->GetLogToConsole() );
+
+    if( this->m_ParameterMapVector.size() > 0 )
+    {
+      ParameterObjectPointer parameterObject = ParameterObjectType::New();
+      parameterObject->SetParameterMap( this->m_ParameterMapVector );
+      elastixFilter->SetParameterObject( parameterObject );
+    }
+
     this->m_ResultImage = Image( elastixFilter->GetOutput() );
+    this->m_TransformParameterMapVector = elastixFilter->GetTransformParameterObject()->GetParameterMap();
   }
   catch( itk::ExceptionObject &e )
   {
     sitkExceptionMacro( << e );
   }
 
-  this->m_ResultImage = Image( elastixFilter->GetOutput() );
-  this->m_TransformParameterMapVector = elastixFilter->GetTransformParameterObject()->GetParameterMap();
-
   // Make a deep copy. This is important to prevent the internal data object trying to update its
   // source (this elastixFilter) outside this function (where it has gone out of scope and been destroyed).
-  // TODO: We should be able to simply call DisconnectPipeline() on the data object but this does not work
+  // TODO: We should be able to simply call DisconnectPipeline() on the data object but this does not seem to work
   this->m_ResultImage.MakeUnique();
 
   return this->m_ResultImage;
--- Code/Elastix/src/sitkSimpleTransformix.hxx
+++ Code/Elastix/src/sitkSimpleTransformix.hxx
@@ -13,45 +13,42 @@ SimpleTransformix::ExecuteInternal( void )
   typedef elastix::TransformixFilter< TInputImage > TransformixFilterType;
   typedef typename TransformixFilterType::Pointer   TransforimxFilterPointer;
 
-  TransforimxFilterPointer transformixFilter = TransformixFilterType::New();
+  try {
+    TransforimxFilterPointer transformixFilter = TransformixFilterType::New();
 
-  if( !this->IsEmpty( this->m_InputImage ) )
-  {
-    transformixFilter->SetInputImage( static_cast< TInputImage* >( this->GetInputImage().GetITKBase() ) );
-  }
+    if (!this->IsEmpty(this->m_InputImage)) {
+      transformixFilter->SetInputImage(static_cast< TInputImage * >( this->GetInputImage().GetITKBase()));
+    }
 
-  transformixFilter->SetInputPointSetFileName( this->GetInputPointSetFileName() );
-  transformixFilter->SetComputeSpatialJacobian( this->GetComputeSpatialJacobian() );
-  transformixFilter->SetComputeDeterminantOfSpatialJacobian( this->GetComputeDeterminantOfSpatialJacobian() );
-  transformixFilter->SetComputeDeformationField( this->GetComputeDeformationField() );
+    transformixFilter->SetInputPointSetFileName(this->GetInputPointSetFileName());
+    transformixFilter->SetComputeSpatialJacobian(this->GetComputeSpatialJacobian());
+    transformixFilter->SetComputeDeterminantOfSpatialJacobian(this->GetComputeDeterminantOfSpatialJacobian());
+    transformixFilter->SetComputeDeformationField(this->GetComputeDeformationField());
 
-  transformixFilter->SetOutputDirectory( this->GetOutputDirectory() );
-  transformixFilter->SetLogFileName( this->GetLogFileName() );
-  transformixFilter->SetLogToFile( this->GetLogToFile() );
-  transformixFilter->SetLogToConsole( this->GetLogToConsole ());
+    transformixFilter->SetOutputDirectory(this->GetOutputDirectory());
+    transformixFilter->SetLogFileName(this->GetLogFileName());
+    transformixFilter->SetLogToFile(this->GetLogToFile());
+    transformixFilter->SetLogToConsole(this->GetLogToConsole());
 
-  ParameterObjectPointer parameterObject = ParameterObjectType::New();
-  parameterObject->SetParameterMap( this->m_TransformParameterMapVector );
-  transformixFilter->SetTransformParameterObject( parameterObject );
+    ParameterObjectPointer parameterObject = ParameterObjectType::New();
+    parameterObject->SetParameterMap(this->m_TransformParameterMapVector);
+    transformixFilter->SetTransformParameterObject(parameterObject);
 
-  try
-  {
     transformixFilter->Update();
+
+    if (!this->IsEmpty(this->m_InputImage)) {
+      this->m_ResultImage = Image(transformixFilter->GetOutput());
+    }
   }
   catch( itk::ExceptionObject &e )
   {
     sitkExceptionMacro( << e );
   }
 
-  if( !this->IsEmpty( this->m_InputImage ) )
-  {
-    this->m_ResultImage = Image( transformixFilter->GetOutput() );
-
-    // Make a deep copy. This is important to prevent the internal data object trying to update its
-    // source (this elastixFilter) outside this function (where it has gone out of scope and been destroyed).
-    // TODO: We should be able to simply call DisconnectPipeline() on the data object but this does not work
-    this->m_ResultImage.MakeUnique();
-  }
+  // Make a deep copy. This is important to prevent the internal data object trying to update its
+  // source (this elastixFilter) outside this function (where it has gone out of scope and been destroyed).
+  // TODO: We should be able to simply call DisconnectPipeline() on the data object but this does not seem to work
+  this->m_ResultImage.MakeUnique();
 
   return this->m_ResultImage;
 }
