1dd325a837911bd6a4cbd33fd59956c70077c7db
Kasper Marstal <kaspermarstal@gmail.com>
2016-02-24 22:09:24 +0100

    BUG: Fix JoinSeriesImageFilter GoogleTest should be fixture
    kaspermarstal/SimpleElastix#14

--- Testing/Unit/sitkImage4DTests.cxx
+++ Testing/Unit/sitkImage4DTests.cxx
@@ -229,7 +229,7 @@ TEST_F(Image4D,Constructors) {
   EXPECT_EQ( image.GetDirection(), directionI4D );
 
   // Test the constructors for vector images
- std::vector<unsigned int>  s4d(4, 5);
+  std::vector<unsigned int>  s4d(4, 5);
 
   image = itk::simple::Image( s4d, itk::simple::sitkVectorUInt8 );
   EXPECT_EQ ( image.GetDimension(), 4u );
@@ -540,7 +540,7 @@ TEST( IO, Image4D )
 }
 
 
-TEST(Image4D, JoinSeriesImageFilter)
+TEST_F(Image4D, JoinSeriesImageFilter)
 {
   std::vector<unsigned int> size(3);
   size[0] = 10;
