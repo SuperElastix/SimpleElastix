2af332a5f695d25c298be4ffe078454c799d17b7
Kasper Marstal <kaspermarstal@gmail.com>
2016-02-24 17:33:44 +0100

    ENH: Increase default finalGridSpacingInPhysicalUnits
    Make default b-spline registration more robust at the expense of
    precision. In the default parameter maps we prioritise robustness.

--- Code/Elastix/include/sitkSimpleElastix.h
+++ Code/Elastix/include/sitkSimpleElastix.h
@@ -108,7 +108,7 @@ class SITKCommon_EXPORT SimpleElastix
     Self& SetParameterMap( const std::map< std::string, std::vector< std::string > > parameterMap );
     Self& AddParameterMap( const std::map< std::string, std::vector< std::string > > parameterMap );
     std::vector< std::map< std::string, std::vector< std::string > > > GetParameterMap( void );
-    std::map< std::string, std::vector< std::string > > GetDefaultParameterMap( const std::string transformName, const unsigned int numberOfResolutions = 4, const double finalGridSpacingInPhysicalUnits = 8.0 );
+    std::map< std::string, std::vector< std::string > > GetDefaultParameterMap( const std::string transformName, const unsigned int numberOfResolutions = 4, const double finalGridSpacingInPhysicalUnits = 10.0 );
     unsigned int GetNumberOfParameterMaps( void );
 
     Self& SetInitialTransformParameterFileName( const std::string initialTransformParmaterFileName );
