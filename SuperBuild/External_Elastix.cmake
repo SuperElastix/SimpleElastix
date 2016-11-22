cf6bf22a8ea1b36f17ec1de3bfce84e8fb2cf779
Kasper Marstal <kaspermarstal@gmail.com>
2016-11-22 21:06:21 +0100

    ENH: Update elastix

--- SuperBuild/External_Elastix.cmake
+++ SuperBuild/External_Elastix.cmake
@@ -11,7 +11,7 @@ endif()
 file( WRITE "${CMAKE_CURRENT_BINARY_DIR}/${proj}-build/CMakeCacheInit.txt" "${ep_common_cache}" )
 
 set( ELASTIX_GIT_REPOSITORY ${git_protocol}://github.com/kaspermarstal/elastix )
-set( ELASTIX_GIT_TAG e29be08c65ee4b204e89ddc144b1f6d931c41fe1 )
+set( ELASTIX_GIT_TAG 99251130b1d04841a1b94f7023be74124d9d9c43 )
 
 if( ${ITK_WRAPPING} OR ${BUILD_SHARED_LIBS} )
   set( ELASTIX_BUILD_SHARED_LIBS ON )
