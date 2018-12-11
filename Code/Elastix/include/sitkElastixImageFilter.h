b5e19c8a0818a92664605507d00b6a2718ca1ddf
Kasper Marstal <kaspermarstal@gmail.com>
2018-12-11 16:27:05 +0100

    STYLE: Fix indentation

--- Code/Elastix/include/sitkElastixImageFilter.h
+++ Code/Elastix/include/sitkElastixImageFilter.h
@@ -12,147 +12,147 @@ namespace itk {
 */
 class SITKCommon_EXPORT ElastixImageFilter
 {
-  public:
+public:
 
-    ElastixImageFilter();
-    ~ElastixImageFilter();
+  ElastixImageFilter();
+  ~ElastixImageFilter();
 
-    typedef ElastixImageFilter Self;                                
+  typedef ElastixImageFilter Self;                                
 
-    typedef std::vector< Image >                                    VectorOfImage;
+  typedef std::vector< Image >                                    VectorOfImage;
 
-    typedef std::string                                             ParameterKeyType;
-    typedef std::string                                             ParameterValueType;
-    typedef std::vector< ParameterValueType >                       ParameterValueVectorType;
-    typedef ParameterValueVectorType::iterator                      ParameterValueVectorIterator;
-    typedef std::map< ParameterKeyType, ParameterValueVectorType >  ParameterMapType;
-    typedef std::vector< ParameterMapType >                         ParameterMapVectorType;
-    typedef ParameterMapType::iterator                              ParameterMapIterator;
-    typedef ParameterMapType::const_iterator                        ParameterMapConstIterator;
+  typedef std::string                                             ParameterKeyType;
+  typedef std::string                                             ParameterValueType;
+  typedef std::vector< ParameterValueType >                       ParameterValueVectorType;
+  typedef ParameterValueVectorType::iterator                      ParameterValueVectorIterator;
+  typedef std::map< ParameterKeyType, ParameterValueVectorType >  ParameterMapType;
+  typedef std::vector< ParameterMapType >                         ParameterMapVectorType;
+  typedef ParameterMapType::iterator                              ParameterMapIterator;
+  typedef ParameterMapType::const_iterator                        ParameterMapConstIterator;
 
-    const std::string GetName();
+  const std::string GetName();
 
-    /** \brief Sets a fixed image.
-     *
-     * @{
-     */
-    Self& SetFixedImage( const Image& fixedImage );
-    /**@}*/
+  /** \brief Sets a fixed image.
+   *
+   * @{
+   */
+  Self& SetFixedImage( const Image& fixedImage );
+  /**@}*/
 
-    /** \brief Sets multiple fixed images.
-     *
-     * @{
-     */
-    Self& SetFixedImage( const VectorOfImage& fixedImages );
-    /**@}*/
+  /** \brief Sets multiple fixed images.
+   *
+   * @{
+   */
+  Self& SetFixedImage( const VectorOfImage& fixedImages );
+  /**@}*/
     
-    // TODO: Add Doxygen documentation for all methods.
-    Self& AddFixedImage( const Image& fixedImage );
+  // TODO: Add Doxygen documentation for all methods.
+  Self& AddFixedImage( const Image& fixedImage );
     
 
-    Image& GetFixedImage( const unsigned long index );
-    VectorOfImage& GetFixedImage();
-    Self& RemoveFixedImage( const unsigned long index );
-    Self& RemoveFixedImage();
-    unsigned int GetNumberOfFixedImages();
+  Image& GetFixedImage( const unsigned long index );
+  VectorOfImage& GetFixedImage();
+  Self& RemoveFixedImage( const unsigned long index );
+  Self& RemoveFixedImage();
+  unsigned int GetNumberOfFixedImages();
 
-    Self& SetMovingImage( const Image& movingImages );
-    Self& SetMovingImage( const VectorOfImage& movingImage );
-    Self& AddMovingImage( const Image& movingImage );
-    Image& GetMovingImage( const unsigned long index );
-    VectorOfImage& GetMovingImage();
-    Self& RemoveMovingImage( const unsigned long index );
-    Self& RemoveMovingImage();
-    unsigned int GetNumberOfMovingImages();
+  Self& SetMovingImage( const Image& movingImages );
+  Self& SetMovingImage( const VectorOfImage& movingImage );
+  Self& AddMovingImage( const Image& movingImage );
+  Image& GetMovingImage( const unsigned long index );
+  VectorOfImage& GetMovingImage();
+  Self& RemoveMovingImage( const unsigned long index );
+  Self& RemoveMovingImage();
+  unsigned int GetNumberOfMovingImages();
 
-    Self& SetFixedMask( const Image& fixedMask );
-    Self& SetFixedMask( const VectorOfImage& fixedMasks );
-    Self& AddFixedMask( const Image& fixedMask );
-    Image& GetFixedMask( const unsigned long index );
-    VectorOfImage& GetFixedMask();
-    Self& RemoveFixedMask( const unsigned long index );
-    Self& RemoveFixedMask();
-    unsigned int GetNumberOfFixedMasks();
+  Self& SetFixedMask( const Image& fixedMask );
+  Self& SetFixedMask( const VectorOfImage& fixedMasks );
+  Self& AddFixedMask( const Image& fixedMask );
+  Image& GetFixedMask( const unsigned long index );
+  VectorOfImage& GetFixedMask();
+  Self& RemoveFixedMask( const unsigned long index );
+  Self& RemoveFixedMask();
+  unsigned int GetNumberOfFixedMasks();
 
-    Self& SetMovingMask( const Image& movingMask );
-    Self& SetMovingMask( const VectorOfImage& movingMasks );
-    Self& AddMovingMask( const Image& movingMask );
-    Image& GetMovingMask( const unsigned long index );
-    VectorOfImage& GetMovingMask();
-    Self& RemoveMovingMask( const unsigned long index );
-    Self& RemoveMovingMask();
-    unsigned int GetNumberOfMovingMasks();
+  Self& SetMovingMask( const Image& movingMask );
+  Self& SetMovingMask( const VectorOfImage& movingMasks );
+  Self& AddMovingMask( const Image& movingMask );
+  Image& GetMovingMask( const unsigned long index );
+  VectorOfImage& GetMovingMask();
+  Self& RemoveMovingMask( const unsigned long index );
+  Self& RemoveMovingMask();
+  unsigned int GetNumberOfMovingMasks();
 
-    Self& SetFixedPointSetFileName( const std::string movingPointSetFileName );
-    std::string GetFixedPointSetFileName();
-    Self& RemoveFixedPointSetFileName();
+  Self& SetFixedPointSetFileName( const std::string movingPointSetFileName );
+  std::string GetFixedPointSetFileName();
+  Self& RemoveFixedPointSetFileName();
 
-    Self& SetMovingPointSetFileName( const std::string movingPointSetFileName );
-    std::string GetMovingPointSetFileName();
-    Self& RemoveMovingPointSetFileName();
+  Self& SetMovingPointSetFileName( const std::string movingPointSetFileName );
+  std::string GetMovingPointSetFileName();
+  Self& RemoveMovingPointSetFileName();
 
-    Self& SetOutputDirectory( const std::string outputDirectory );
-    std::string GetOutputDirectory();
-    Self& RemoveOutputDirectory();
+  Self& SetOutputDirectory( const std::string outputDirectory );
+  std::string GetOutputDirectory();
+  Self& RemoveOutputDirectory();
 
-    Self& SetLogFileName( const std::string logFileName );
-    std::string GetLogFileName();
-    Self& RemoveLogFileName();
+  Self& SetLogFileName( const std::string logFileName );
+  std::string GetLogFileName();
+  Self& RemoveLogFileName();
 
-    Self& SetLogToFile( const bool logToFile );
-    bool GetLogToFile();
-    Self& LogToFileOn();
-    Self& LogToFileOff();
+  Self& SetLogToFile( const bool logToFile );
+  bool GetLogToFile();
+  Self& LogToFileOn();
+  Self& LogToFileOff();
 
-    Self& SetLogToConsole( bool );
-    bool GetLogToConsole();
-    Self& LogToConsoleOn();
-    Self& LogToConsoleOff();
+  Self& SetLogToConsole( bool );
+  bool GetLogToConsole();
+  Self& LogToConsoleOn();
+  Self& LogToConsoleOff();
 
-    Self& SetNumberOfThreads( int n );
-    int GetNumberOfThreads();
+  Self& SetNumberOfThreads( int n );
+  int GetNumberOfThreads();
 
-    Self& SetParameterMap( const std::string transformName, const unsigned int numberOfResolutions = 4u, const double finalGridSpacingInPhysicalUnits = 10.0 );
-    Self& SetParameterMap( const std::vector< std::map< std::string, std::vector< std::string > > > parameterMapVector );
-    Self& SetParameterMap( const std::map< std::string, std::vector< std::string > > parameterMap );
-    Self& AddParameterMap( const std::map< std::string, std::vector< std::string > > parameterMap );
-    std::vector< std::map< std::string, std::vector< std::string > > > GetParameterMap();
-    std::map< std::string, std::vector< std::string > > GetDefaultParameterMap( const std::string transformName, const unsigned int numberOfResolutions = 4, const double finalGridSpacingInPhysicalUnits = 10.0 );
-    unsigned int GetNumberOfParameterMaps();
+  Self& SetParameterMap( const std::string transformName, const unsigned int numberOfResolutions = 4u, const double finalGridSpacingInPhysicalUnits = 10.0 );
+  Self& SetParameterMap( const std::vector< std::map< std::string, std::vector< std::string > > > parameterMapVector );
+  Self& SetParameterMap( const std::map< std::string, std::vector< std::string > > parameterMap );
+  Self& AddParameterMap( const std::map< std::string, std::vector< std::string > > parameterMap );
+  std::vector< std::map< std::string, std::vector< std::string > > > GetParameterMap();
+  std::map< std::string, std::vector< std::string > > GetDefaultParameterMap( const std::string transformName, const unsigned int numberOfResolutions = 4, const double finalGridSpacingInPhysicalUnits = 10.0 );
+  unsigned int GetNumberOfParameterMaps();
 
-    Self& SetParameter( const std::string key, const std::string value );
-    Self& SetParameter( const std::string key, const std::vector< std::string > value );
-    Self& SetParameter( const unsigned int index, const std::string key, const std::string value );
-    Self& SetParameter( const unsigned int index, const std::string key, const std::vector< std::string > value );
-    Self& AddParameter( const std::string key, const std::string value );
-    Self& AddParameter( const unsigned int index, const std::string key, const std::string value );
-    Self& AddParameter( const std::string key, const std::vector< std::string > value );
-    Self& AddParameter( const unsigned int index, const std::string key, const std::vector< std::string > value );
-    std::vector< std::string > GetParameter( const std::string key );
-    std::vector< std::string > GetParameter( const unsigned int index, const std::string key );
-    Self& RemoveParameter( const std::string key );
-    Self& RemoveParameter( const unsigned int index, const std::string key );
+  Self& SetParameter( const std::string key, const std::string value );
+  Self& SetParameter( const std::string key, const std::vector< std::string > value );
+  Self& SetParameter( const unsigned int index, const std::string key, const std::string value );
+  Self& SetParameter( const unsigned int index, const std::string key, const std::vector< std::string > value );
+  Self& AddParameter( const std::string key, const std::string value );
+  Self& AddParameter( const unsigned int index, const std::string key, const std::string value );
+  Self& AddParameter( const std::string key, const std::vector< std::string > value );
+  Self& AddParameter( const unsigned int index, const std::string key, const std::vector< std::string > value );
+  std::vector< std::string > GetParameter( const std::string key );
+  std::vector< std::string > GetParameter( const unsigned int index, const std::string key );
+  Self& RemoveParameter( const std::string key );
+  Self& RemoveParameter( const unsigned int index, const std::string key );
 
-    Self& SetInitialTransformParameterFileName( const std::string initialTransformParmaterFileName );
-    std::string GetInitialTransformParameterFileName();
-    Self& RemoveInitialTransformParameterFileName();
+  Self& SetInitialTransformParameterFileName( const std::string initialTransformParmaterFileName );
+  std::string GetInitialTransformParameterFileName();
+  Self& RemoveInitialTransformParameterFileName();
 
-    std::map< std::string, std::vector< std::string > > ReadParameterFile( const std::string filename );
-    Self& WriteParameterFile( const std::map< std::string, std::vector< std::string > > parameterMap, const std::string filename );
+  std::map< std::string, std::vector< std::string > > ReadParameterFile( const std::string filename );
+  Self& WriteParameterFile( const std::map< std::string, std::vector< std::string > > parameterMap, const std::string filename );
     
-    Image Execute();
-    std::vector< std::map< std::string, std::vector< std::string > > > GetTransformParameterMap();
-    std::map< std::string, std::vector< std::string > > GetTransformParameterMap( const unsigned int index );
-    Image GetResultImage();
+  Image Execute();
+  std::vector< std::map< std::string, std::vector< std::string > > > GetTransformParameterMap();
+  std::map< std::string, std::vector< std::string > > GetTransformParameterMap( const unsigned int index );
+  Image GetResultImage();
 
-    Self& PrintParameterMap();
-    Self& PrintParameterMap( const ParameterMapType parameterMapVector );
-    Self& PrintParameterMap( const ParameterMapVectorType parameterMapVector );
+  Self& PrintParameterMap();
+  Self& PrintParameterMap( const ParameterMapType parameterMapVector );
+  Self& PrintParameterMap( const ParameterMapVectorType parameterMapVector );
 
-  private:
+private:
 
-    struct ElastixImageFilterImpl;
-    ElastixImageFilterImpl* m_Pimple;
+  struct ElastixImageFilterImpl;
+  ElastixImageFilterImpl* m_Pimple;
     
 };
 
--- Code/Elastix/include/sitkTransformixImageFilter.h
+++ Code/Elastix/include/sitkTransformixImageFilter.h
@@ -9,98 +9,98 @@ namespace itk {
 
 class SITKCommon_EXPORT TransformixImageFilter
 {
-  public:
+public:
 
-    TransformixImageFilter();
-    ~TransformixImageFilter();
+  TransformixImageFilter();
+  ~TransformixImageFilter();
 
-    typedef TransformixImageFilter Self;                                
+  typedef TransformixImageFilter Self;                                
 
-    typedef std::string                                             ParameterKeyType;
-    typedef std::string                                             ParameterValueType;
-    typedef std::vector< ParameterValueType >                       ParameterValueVectorType;
-    typedef ParameterValueVectorType::iterator                      ParameterValueVectorIterator;
-    typedef std::map< ParameterKeyType, ParameterValueVectorType >  ParameterMapType;
-    typedef std::vector< ParameterMapType >                         ParameterMapVectorType;
-    typedef ParameterMapType::iterator                              ParameterMapIterator;
-    typedef ParameterMapType::const_iterator                        ParameterMapConstIterator;
+  typedef std::string                                             ParameterKeyType;
+  typedef std::string                                             ParameterValueType;
+  typedef std::vector< ParameterValueType >                       ParameterValueVectorType;
+  typedef ParameterValueVectorType::iterator                      ParameterValueVectorIterator;
+  typedef std::map< ParameterKeyType, ParameterValueVectorType >  ParameterMapType;
+  typedef std::vector< ParameterMapType >                         ParameterMapVectorType;
+  typedef ParameterMapType::iterator                              ParameterMapIterator;
+  typedef ParameterMapType::const_iterator                        ParameterMapConstIterator;
 
-    const std::string GetName();
+  const std::string GetName();
 
-    Self& SetMovingImage( const Image& movingImage );
-    Image& GetMovingImage();
-    Self& RemoveMovingImage();
+  Self& SetMovingImage( const Image& movingImage );
+  Image& GetMovingImage();
+  Self& RemoveMovingImage();
 
-    Self& SetFixedPointSetFileName( const std::string movingPointSetFileName );
-    std::string GetFixedPointSetFileName();
-    Self& RemoveFixedPointSetFileName();
+  Self& SetFixedPointSetFileName( const std::string movingPointSetFileName );
+  std::string GetFixedPointSetFileName();
+  Self& RemoveFixedPointSetFileName();
 
-    Self& SetComputeSpatialJacobian( const bool );
-    bool GetComputeSpatialJacobian();
-    Self& ComputeSpatialJacobianOn();
-    Self& ComputeSpatialJacobianOff();
+  Self& SetComputeSpatialJacobian( const bool );
+  bool GetComputeSpatialJacobian();
+  Self& ComputeSpatialJacobianOn();
+  Self& ComputeSpatialJacobianOff();
 
-    Self& SetComputeDeterminantOfSpatialJacobian( const bool );
-    bool GetComputeDeterminantOfSpatialJacobian();
-    Self& ComputeDeterminantOfSpatialJacobianOn();
-    Self& ComputeDeterminantOfSpatialJacobianOff();
+  Self& SetComputeDeterminantOfSpatialJacobian( const bool );
+  bool GetComputeDeterminantOfSpatialJacobian();
+  Self& ComputeDeterminantOfSpatialJacobianOn();
+  Self& ComputeDeterminantOfSpatialJacobianOff();
 
-    Self& SetComputeDeformationField( bool );
-    bool GetComputeDeformationField();
-    Self& ComputeDeformationFieldOn();
-    Self& ComputeDeformationFieldOff();
+  Self& SetComputeDeformationField( bool );
+  bool GetComputeDeformationField();
+  Self& ComputeDeformationFieldOn();
+  Self& ComputeDeformationFieldOff();
 
-    Self& SetOutputDirectory( const std::string outputDirectory );
-    std::string GetOutputDirectory();
-    Self& RemoveOutputDirectory();
+  Self& SetOutputDirectory( const std::string outputDirectory );
+  std::string GetOutputDirectory();
+  Self& RemoveOutputDirectory();
 
-    Self& SetLogFileName( const std::string logFileName );
-    std::string GetLogFileName();
-    Self& RemoveLogFileName();
+  Self& SetLogFileName( const std::string logFileName );
+  std::string GetLogFileName();
+  Self& RemoveLogFileName();
 
-    Self& SetLogToFile( const bool logToFile );
-    bool GetLogToFile();
-    Self& LogToFileOn();
-    Self& LogToFileOff();
+  Self& SetLogToFile( const bool logToFile );
+  bool GetLogToFile();
+  Self& LogToFileOn();
+  Self& LogToFileOff();
 
-    Self& SetLogToConsole( const bool logToConsole );
-    bool GetLogToConsole();
-    Self& LogToConsoleOn();
-    Self& LogToConsoleOff();
+  Self& SetLogToConsole( const bool logToConsole );
+  bool GetLogToConsole();
+  Self& LogToConsoleOn();
+  Self& LogToConsoleOff();
 
-    Self& SetTransformParameterMap( const std::vector< std::map< std::string, std::vector< std::string > > > parameterMapVector );
-    Self& SetTransformParameterMap( const std::map< std::string, std::vector< std::string > > parameterMap );
-    Self& AddTransformParameterMap( const std::map< std::string, std::vector< std::string > > parameterMap );
-    std::vector< std::map< std::string, std::vector< std::string > > > GetTransformParameterMap();
-    unsigned int GetNumberOfTransformParameterMaps();
+  Self& SetTransformParameterMap( const std::vector< std::map< std::string, std::vector< std::string > > > parameterMapVector );
+  Self& SetTransformParameterMap( const std::map< std::string, std::vector< std::string > > parameterMap );
+  Self& AddTransformParameterMap( const std::map< std::string, std::vector< std::string > > parameterMap );
+  std::vector< std::map< std::string, std::vector< std::string > > > GetTransformParameterMap();
+   unsigned int GetNumberOfTransformParameterMaps();
 
-    Self& SetTransformParameter( const std::string key, const std::string value );
-    Self& SetTransformParameter( const std::string key, const std::vector< std::string > value );
-    Self& SetTransformParameter( const unsigned int index, const std::string key, const std::string value );
-    Self& SetTransformParameter( const unsigned int index, const std::string key, const std::vector< std::string > value );
-    Self& AddTransformParameter( const std::string key, const std::string value );
-    Self& AddTransformParameter( const unsigned int index, const std::string key, const std::string value );
-    std::vector< std::string > GetTransformParameter( const std::string key );
-    std::vector< std::string > GetTransformParameter( const unsigned int index, const std::string key );
-    Self& RemoveTransformParameter( const std::string key );
-    Self& RemoveTransformParameter( const unsigned int index, const std::string key );
+  Self& SetTransformParameter( const std::string key, const std::string value );
+  Self& SetTransformParameter( const std::string key, const std::vector< std::string > value );
+  Self& SetTransformParameter( const unsigned int index, const std::string key, const std::string value );
+  Self& SetTransformParameter( const unsigned int index, const std::string key, const std::vector< std::string > value );
+  Self& AddTransformParameter( const std::string key, const std::string value );
+  Self& AddTransformParameter( const unsigned int index, const std::string key, const std::string value );
+  std::vector< std::string > GetTransformParameter( const std::string key );
+  std::vector< std::string > GetTransformParameter( const unsigned int index, const std::string key );
+  Self& RemoveTransformParameter( const std::string key );
+  Self& RemoveTransformParameter( const unsigned int index, const std::string key );
 
-    std::map< std::string, std::vector< std::string > > ReadParameterFile( const std::string parameterFileName );
-    Self& WriteParameterFile( const std::map< std::string, std::vector< std::string > > parameterMap, const std::string parameterFileName );
+  std::map< std::string, std::vector< std::string > > ReadParameterFile( const std::string parameterFileName );
+  Self& WriteParameterFile( const std::map< std::string, std::vector< std::string > > parameterMap, const std::string parameterFileName );
 
-    Self& PrintParameterMap();
-    Self& PrintParameterMap( const std::map< std::string, std::vector< std::string > > parameterMap );
-    Self& PrintParameterMap( const std::vector< std::map< std::string, std::vector< std::string > > > parameterMapVector );
+  Self& PrintParameterMap();
+  Self& PrintParameterMap( const std::map< std::string, std::vector< std::string > > parameterMap );
+  Self& PrintParameterMap( const std::vector< std::map< std::string, std::vector< std::string > > > parameterMapVector );
 
-    Image Execute();
+  Image Execute();
 
-    Image GetResultImage();
-    Image GetDeformationField();
+  Image GetResultImage();
+  Image GetDeformationField();
 
-  private:
+private:
 
-    struct TransformixImageFilterImpl;
-    TransformixImageFilterImpl* m_Pimple;
+  struct TransformixImageFilterImpl;
+  TransformixImageFilterImpl* m_Pimple;
     
 };
 
